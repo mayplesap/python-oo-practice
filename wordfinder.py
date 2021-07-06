9458eaada3a8b9b900473d8c8bae595e4951f419
Wukongthethird <simonzhang9518@gmail.com>
2021-07-06 15:50:02 -0700

    wordfinder class

--- wordfinder.py
+++ wordfinder.py
@@ -1,2 +1,28 @@
+from random import choice
 class WordFinder:
     """Word Finder: finds random words from a dictionary."""
+    def __init__(self, file_name ):
+        """"""
+        self.file_name = file_name 
+        self.list = self.read_and_create_list()
+
+
+    def read_and_create_list(self):
+        """"""
+        curr_list = []
+        file = open(self.file_name)
+        for line in file:
+            curr_list.append(line[ :-1])
+
+        file.close()
+        
+        return curr_list
+
+
+    def random(self):
+        """"""
+        word = choice(self.list)
+        return word
+
+
+
